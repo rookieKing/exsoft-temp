4c074c8076844233a0f4e0da7ff896395a36e262
rookieking <rookielei@gamil.com>
2016-08-18 16:14:29 +0800

    temp
    Signed-off-by: rookieking <rookielei@gamil.com>

---
+++ Scripts/mobile-play.js
@@ -0,0 +1,92 @@
+﻿$(function () {
+    //#region 节目信息
+    var brief = $("#brief"),
+        chapter = $("#chapter"),
+        play = $(".play:first"),
+        processor = {
+            timeoutId: null,
+            performProcessor: function () {
+                play.css({
+                    width: $(window).width(),
+                    height: $(window).height() - 24
+                });
+                //$(window).scrollTop(play.offset().top);
+                //todo flash大小改变
+
+                //#region 详细信息
+                if (!brief.data("animate")) {
+                    $("#brief").css("height", "auto");
+                    var briefHeight = brief.height();
+                    $("#extendbrief").off("click");
+                    if (briefHeight > 48) {
+                        $("#brief").css("margin-bottom", "1.5em");
+                        $("#extendbrief").parent().show();
+                        $("#extendbrief").on("click", function () {
+                            if ($(this).text() == "收起简介") {
+                                brief.data("animate", true).animate({ height: 48 }, 500, function () {
+                                    brief.data("animate", false);
+                                });
+                                $(this).data("show", false).text("展开简介").parent().removeClass("hide");
+                            } else {
+                                brief.css("height", "auto");
+                                $(this).data("show", true).text("收起简介").parent().addClass("hide");
+                            }
+                            $(window).scrollTop($("#brief").offset().top);
+                        });
+                        brief.height(48);
+
+                    } else {
+                        $("#extendbrief").parent().hide();
+                        $("#brief").css("margin-bottom", "0px");
+                        $("#extendbrief").off("click");
+                    }
+
+                    if ($("#extendbrief").data("show")) {
+                        brief.css("height", "auto");
+                        $("#extendbrief").text("收起简介").parent().addClass("hide");
+                    } else {
+                        brief.css("height", "48px");
+                        $("#extendbrief").text("展开简介").parent().removeClass("hide");
+                    }
+                }
+                //#endregion
+
+                //#region 片断
+                $("#chapter").css("height", "auto");
+                var chapterHeight = chapter.height();
+                $("#extendchapter").off("click");
+                if (chapterHeight > 150) {
+                    $("#extendchapter").css("display", "inline-block");
+                    chapter.height(150);
+                    $("#extendchapter").on("click", function () {
+                        $(this).data("show", true);
+                        chapter.css("height", "auto");
+                        $(this).hide();
+                    });
+                } else {
+                    $("#extendchapter").hide();
+                    $("#extendchapter").off("click");
+                }
+
+                if ($("#extendchapter").data("show")) {
+                    $("#extendchapter").click();
+                } else {
+
+                }
+                //#endregion
+            },
+            process: function () {
+                clearTimeout(this.timeoutId);
+                var that = this;
+                this.timeoutId = setTimeout(function () {
+                    that.performProcessor();
+                }, 100);
+            }
+        };
+    processor.process();
+    $(window).resize(function () {
+        console.log($(window).height());
+        processor.process();
+    });
+    //#endregion
+});
---
+++ Scripts/mobile-search.js
@@ -0,0 +1,40 @@
+﻿(function ($) {
+    var searchCondition = $("#searchCondition");
+    $("#conditionSwitch").click(function () {
+        searchCondition.toggleClass("hide");
+    }); 
+
+    var processor = {
+        timeoutId: null,
+        performProcessor: function (e) {
+            if ($(document).height() <= $(window).scrollTop() + $(window).height() + 100) {
+                //加载更多-如果有更多的话
+                console.log(new Date().toLocaleString());
+
+            }
+        },
+        process: function (e) {
+            clearTimeout(this.timeoutId);
+
+            var that = this;
+            this.timeoutId = setTimeout(function () {
+                that.performProcessor(e);
+            }, 100);
+        }
+    };
+
+    $(window).scroll(function (e) {
+        processor.process.call(processor, e);
+    });
+
+    //$.get("/U/Search", {
+    //    keyword: "",
+    //    categoryId: "",
+    //    year: "",
+    //    sDuration: "",
+    //    eDuration: "",
+    //    order: ""
+    //}, function (data) {
+    //    console.log(data);
+    //}, "html");
+})(jQuery);
